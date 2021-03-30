b8fb258f7ca37a4659e944899d945c88679a6caa
S Santosh Kumar <chrymsler@gmail.com>
2021-03-30 20:34:43 +0530

    subcription based graphql interaction with dgraph working example

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,39 @@
 <script>
-	import { ApolloClient, InMemoryCache } from "@apollo/client";
+	import {
+		ApolloClient,
+		InMemoryCache,
+		HttpLink,
+		split,
+	} from "@apollo/client";
+	import { getMainDefinition } from "@apollo/client/utilities";
+	import { WebSocketLink } from "@apollo/link-ws";
 	import { setClient } from "svelte-apollo";
 
-	const client = new ApolloClient({
+	const httpLink = new HttpLink({
 		uri: "https://billowing-bird.ap-south-1.aws.cloud.dgraph.io/graphql",
+	});
+
+	const wsLink = new WebSocketLink({
+		uri: "wss://billowing-bird.ap-south-1.aws.cloud.dgraph.io/graphql",
+		options: {
+			reconnect: true,
+		},
+	});
+
+	const splitLink = split(
+		({ query }) => {
+			const definition = getMainDefinition(query);
+			return (
+				definition.kind === "OperationDefinition" &&
+				definition.operation === "subscription"
+			);
+		},
+		wsLink,
+		httpLink
+	);
+
+	const client = new ApolloClient({
+		link: splitLink,
 		cache: new InMemoryCache(),
 		onError: ({ networkError, graphQLErrors }) => {
 			console.log("graphQLErrors", graphQLErrors);
--- src/components/SubscribeApp.svelte
+++ src/components/SubscribeApp.svelte
@@ -1,3 +1,41 @@
-<main>
-    Subscribe App
-</main>
+<script>
+    import { gql } from "@apollo/client";
+    import { subscribe } from "svelte-apollo";
+    import { dataset_dev } from "svelte/internal";
+    import App from "../App.svelte";
+    import Hoverable from "./Hoverable.svelte";
+
+    const sProjectsQuery = gql`
+        subscription AllSDataQuery {
+            querySProject {
+                id
+                name
+                stasks {
+                    id
+                    name
+                    status
+                    ssubTasks {
+                        id
+                        name
+                        status
+                    }
+                }
+            }
+        }
+    `;
+
+    const projects = subscribe(sProjectsQuery, { fetchPolicy: "network-only" });
+</script>
+
+<main>Subscribe App</main>
+{#if $projects.loading}
+    Loading...
+{:else if $projects.error}
+    Oh no! {$projects.error.message}
+{:else if $projects.data}
+    <ol>
+        {#each $projects.data.querySProject as project}
+            <li>{project.name}</li>
+        {/each}
+    </ol>
+{/if}
